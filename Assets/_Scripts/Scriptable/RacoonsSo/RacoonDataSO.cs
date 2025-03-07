b5fcc136e6abc8f76a97a2988c7c56a6c71f4208
usualnik <usualnik@gmail.com>
2025-03-07 13:02:19 +0300

    Remove cached

--- Assets/_Scripts/Scriptable/RacoonsSo/RacoonDataSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "ScriptableObject/RacoonDataSO")]
-public class RacoonDataSO : ScriptableObject
-{
-    public string RacoonName;
-    public GameObject RacoonPrefab;
-    
-}
